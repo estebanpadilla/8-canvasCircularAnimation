4521e294ba4bd0d7d081fad9bae00a23614677ee
Esteban Padilla <ep@estebanpadilla.com>
2017-01-10 17:33:14 -0600

    Save.

--- js/canvas.js
+++ js/canvas.js
@@ -86,3 +86,21 @@ function createCanvas(width, height) {
     return canvas;
 }
 
+function Ball(x, y, radius, color, context) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.context = context;
+}
+
+Ball.prototype.render = function render() {
+    this.context.fillStyle = this.color;
+    this.context.beginPath();
+    this.context.arc(this.x, this.y, this.radius, 0, (2 * Math.PI), false);
+    this.context.fill();
+    this.context.closePath();
+}
+Ball.prototype.update = function () {
+    this.render();
+}
